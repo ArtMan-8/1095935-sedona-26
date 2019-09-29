eb5bf0e100c3f726e17c2457f09a6424119cb2f1
Art <ya.artriches@ya.ru>
2019-09-29 19:09:25 +0300

    переосмысление карты и фомы

--- js/search-hotel.js
+++ js/search-hotel.js
@@ -1,35 +1,10 @@
-function init() {
-  var e = {
-    zoom: 9,
-    mapTypeControl: !1,
-    zoomControl: !0,
-    scrollwheel: !1,
-    zoomControlOptions: {
-      position: google.maps.ControlPosition.LEFT_TOP
-    },
-    streetViewControl: !1,
-    center: new google.maps.LatLng(34.831793, -111.762655)
-  };
-  map = new google.maps.Map(mapElement, e);
-  new google.maps.Marker({
-    position: new google.maps.LatLng(34.869977, -111.760947),
-    map: map,
-    optimized: !1
-  });
-  m_res()
-}
+var	buttonShow = document.querySelector(".form-search-show");
+var formSearch = document.querySelector(".form-search");
+var close = formSearch.querySelector(".form-search--show");
 
-function m_res() {
-  google.maps.event.trigger(map, "resize"), map.panTo(new google.maps.LatLng(34.831793, -111.762655))
-}
+formSearch.classList.add('form-search--hide');
 
-var mapElement = document.getElementById("map"),
-  formOpen = document.querySelector(".form-search-show"),
-  formSearch = document.querySelector(".form-search");
-
-if (formOpen && (formSearch && formSearch.classList.add("form-search--show"), formOpen.addEventListener("click", function (e) {
-    e.preventDefault(), formSearch.classList.toggle("form-search--show"), formSearch.classList.toggle("form-search--hide")
-  })), mapElement) {
-  var map = "";
-  google.maps.event.addDomListener(window, "load", init), google.maps.event.addDomListener(window, "resize", m_res)
-}
+buttonShow.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  formSearch.classList.toggle("form-search--hide");
+});
