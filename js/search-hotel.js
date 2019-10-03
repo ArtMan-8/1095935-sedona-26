60ecbc8a77ebd93078c5629bdf8e6f356c5194fc
Art <ya.artriches@ya.ru>
2019-10-03 19:10:32 +0300

    мелкие правки

--- js/search-hotel.js
+++ js/search-hotel.js
@@ -1,6 +1,5 @@
 var	buttonShow = document.querySelector(".form-search-show");
 var formSearch = document.querySelector(".form-search");
-var close = formSearch.querySelector(".form-search--show");
 
 formSearch.classList.add('form-search--hide');
 
